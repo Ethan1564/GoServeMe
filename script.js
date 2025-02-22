e7d65a074ffc5cfd072f3c00bd4d1e7e5c71db9f
Ethan1564 <escher.ethan09@gmail.com>
2025-02-21 20:28:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function searchOpportunities() {
+    const location = document.getElementById('location').value;
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = ''; // Clear previous results
 
+    if (location) {
+        // Example: Displaying a static message for demonstration purposes
+        resultsDiv.innerHTML = `
+            <h3>Volunteer Opportunities Near ${location}:</h3>
+            <ul>
+                <li>Volunteer at Local Shelter</li>
+                <li>Clean Up the Park</li>
+                <li>Teach English to Refugees</li>
+            </ul>
+        `;
+    } else {
+        resultsDiv.innerHTML = '<p>Please enter a location to search.</p>';
+    }
+}
